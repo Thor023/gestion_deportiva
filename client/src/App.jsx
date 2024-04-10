cc4b1236c1d41301c317f9375f04d59a911e5953
Thor023 <felipe.castaneda84@gmail.com>
2024-04-09 20:26:07 -0400

    router y paginas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import About from './pages/About';
+import SingIn from './pages/SingIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
 
 export default function App() {
   return (
-    <h1 className='text-red-600'>APP</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SingIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
